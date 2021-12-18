f184ef5f2c35bb57d555db93e338822db70afff3
RicoWilliams <ricowilliams@protonmail.com>
2021-12-18 14:46:36 -0700

    route tested and working

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const db = require('./db');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const controller = require('./controllers/Controller.js');
-const { Router } = require('express');
-const routes = require('./router/index.js');
+// const logger = require('morgan');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const controller = require('./controllers/Controller.js');
+// const { Router } = require('express');
+const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(logger('dev'));
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(logger('dev'));
 
 app.use('/api', routes);
 
