a3223e532a0d822168e86d8c6b546e0a6efebeff
RicoWilliams <ricowilliams@protonmail.com>
2021-12-20 10:37:19 -0700

    Trail input form created

---
+++ client/src/Trailinput.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import TrailArray from './data/trails';
+import axios from 'axios';
+const BASE_URL = 'http://localhost:3001/api';
+
+const FormInput = (props) => {
+  const [data, setData] = useState(TrailArray);
+  const [Trails, setTrail] = useState(data);
+  const [newTrail, setNewTrail] = useState({
+    name: '',
+    image: '',
+    area: '',
+    difficulty: '',
+    details: ''
+  });
+};
+
+const allTrails = async () => {
+  const res = await axios.post(`${BASE_URL}/createtrails, newTrail`);
+  setTrail(res.data.trails);
+  console.log(data);
+};
+
+const addTrail = (e) => {
+  e.preventDefault();
+  const currentTrail = Trails;
+  const newestTrail = newTrail;
+  currentTrail.push(newestTrail);
+  setTrail(currentTrail);
+  setNewTrail({
+    name: '',
+    image: '',
+    area: '',
+    difficulty: '',
+    details: ''
+  });
+};
+
+const handleChange = (e) => {
+  setNewTrail({
+    ...newTrail,
+    [e.target.name]: e.target.value
+  });
+};
+
+const formSubmit = (e) => {
+  addTrail(e);
+  allTrails();
+
+  console.log(newTrail);
+};
+
+return (
+  <div>
+    <h1>Add New Trail</h1>
+    <form onsubmit={formSubmit}>
+      <input
+        type="text"
+        value={newTrail.name}
+        onChange={handleChange}
+        name={'name'}
+        placeholder={'Trail Name'}
+      />
+      <input
+        type="text"
+        value={newTrail.img}
+        onChange={handleChange}
+        name={'image'}
+        placeholder={'image'}
+      />
+      <input
+        type="text"
+        value={newTrail.area}
+        onChange={handleChange}
+        name={'area'}
+        placeholder={'Location'}
+      />
+      <input
+        type="text"
+        value={newTrail.difficulty}
+        onChange={handleChange}
+        name={'difficulty'}
+        placeholder={'difficulty'}
+      />
+      <input
+        type="text"
+        value={newTrail.details}
+        onChange={handleChange}
+        name={'details'}
+        placeholder={'descirption'}
+      />
+    </form>
+  </div>
+);
+
+export default FormInput;
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,8 @@ const router = Router();
 
 router.get('/', (req, res) => res.send('This is root!'));
 router.get('/getalltrails', controller.getAllTrails);
-router.get('/trails', controller.createTrail);
-router.get('/trails/:id', controller.updateTrail);
-router.get('/trails/:id', controller.deleteTrail);
+router.post('/createtrails', controller.createTrail);
+router.get('/updatetrailstrails/:id', controller.updateTrail);
+router.get('/deletetrails/:id', controller.deleteTrail);
 
 module.exports = router;
