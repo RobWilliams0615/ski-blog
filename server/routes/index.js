9f1cf4e5684d9767043324b87ca0d24c6cc32db8
RicoWilliams <ricowilliams@protonmail.com>
2021-12-20 12:35:40 -0700

    fixed router.methods on routes/index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ const router = Router();
 router.get('/', (req, res) => res.send('This is root!'));
 router.get('/getalltrails', controller.getAllTrails);
 router.post('/createtrails', controller.createTrail);
-router.get('/updatetrailstrails/:id', controller.updateTrail);
-router.get('/deletetrails/:id', controller.deleteTrail);
+router.put('/updatetrailstrails/:id', controller.updateTrail);
+router.delete('/deletetrails/:id', controller.deleteTrail);
 
 module.exports = router;
