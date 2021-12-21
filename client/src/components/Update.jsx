60d71a96fbc5c47042e169ca16c8fbd4fa0d7755
RicoWilliams <ricowilliams@protonmail.com>
2021-12-21 07:52:48 -0700

    add functions to Update componenet

--- client/src/components/Update.jsx
+++ client/src/components/Update.jsx
@@ -1,45 +1,89 @@
 import React from 'React';
+import TrailArray from '../data/trails'
+import axios from 'axios'
+const BASE_URL = 'http://localhost:3001/api'
 
+const FormInput = (props) => {
+  const [data, setData] = useState(TrailArray);
+  const [Trails, setTrail] = useState(data);
+  const [updatedTrail, setUpdatedTrail] = useState({
+    name: '',
+    image: '',
+    area: '',
+    difficulty: '',
+    details: ''
+  });
 
+  const updateTrails = async () => {
+    const res = await axios.post(`${BASE_URL}/updatetrails`, updatedTrail);
+    setData(res.data.trails);
+    console.log(data);
+  };
 
-function Update() {
-  return (
+  const updateTrail = (e) => {
+    e.preventDefault();
+    const currentTrail = Trails;
+    const changedTrail = updatedTrail;
+    currentTrail.push(changedTrail);
+    setTrail(currentTrail);
+    setUpdatedTrail({
+      name: '',
+      image: '',
+      area: '',
+      difficulty: '',
+      details: ''
+    });
+  };
+const handleChange = (e) => {
+  setUpdatedTrail({
+    ...updatedTrail,
+    [e.target.name]: e.target.value
+  });
+};
 
+const formSubmit = (e) => {
+  updateTrail(e);
+  updateTrails();
+
+  console.log(updatedTrail);
+};
+
+return (
 <div>
-
+<h2>Update Trail</h2>
 
 <form onSubmit={formSubmit}>
         <input
           type="text"
-          value={newTrail.name}
+          value={updatedTrail.name}
           onChange={handleChange}
           name={'name'}
           placeholder={'Trail Name'}
         />
         <input
           type="text"
-          value={newTrail.img}
+          value={updatedTrail.img}
           onChange={handleChange}
           name={'image'}
           placeholder={'image'}
         />
         <input
           type="text"
-          value={newTrail.area}
+          value={updatedTrail.area}
           onChange={handleChange}
           name={'area'}
           placeholder={'Location'}
         />
         <input
           type="text"
-          value={newTrail.difficulty}
+          value={updatedTrail.difficulty}
           onChange={handleChange}
           name={'difficulty'}
           placeholder={'difficulty'}
         />
         <input
           type="text"
-          value={newTrail.details}
+          value={updatedTrail.details}
           onChange={handleChange}
           name={'details'}
           placeholder={'descirption'}
@@ -47,6 +91,7 @@ function Update() {
         <button>Update Post</button>
       </form>
       </div>
-    )}
+    )
+  }
 
 export default Update;
